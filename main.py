be437059ee02cf227da8597af22672631c186250
M.S.J Hafiz <msjhafiz@users.noreply.github.com>
2026-08-13 11:51:09 +0000

    Bump app version to v18

--- main.py
+++ main.py
@@ -437,7 +437,7 @@ def api_status():
     state, mins = market_state()
     return JSONResponse(
         {
-            "version": "v17",
+            "version": "v18",
             "mode": config.DATA_MODE,
             "watchlist": config.WATCHLIST,
             "market_state": state,
